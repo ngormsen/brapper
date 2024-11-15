1bdac34e073a162dc83f99d38327bb0c3349b3a7
Nils Gormsen <nils.gormsen@googlemail.com>
2024-11-15 23:33:24 +0100

    Added dotenv configuration, updated package dependencies, and implemented a basic thought navigation  in React with TypeScript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,197 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { AccessType, createLink, createThought, getThought, getThoughtByExactName, getThoughtDetails, ROOT_THOUGHT_ID, ThoughtKind, ThoughtRelation } from './Client';
+
+
+type Thought = {
+  id?: string;
+  name: string;
+  children?: Thought[];
+}
 
 function App() {
+  const [parent, setParent] = useState<Thought>({
+    id: ROOT_THOUGHT_ID,
+    name: "API_Brain",
+  });
+  const [children, setChildren] = useState<Thought[]>([]);
+  const [thoughtCandidate, setThoughtCandidate] = useState<Thought | null>(null);
+
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  useEffect(() => {
+    getThoughtDetails(ROOT_THOUGHT_ID).then((thought) => {
+      console.log(thought);
+      setChildren(thought.children);
+      setParent({
+        id: thought.activeThought.id,
+        name: thought.activeThought.name,
+        children: [],
+      });
+    })
+  }, [])
+
+
+  const navigateToThought = (thought: Thought) => {
+    console.log(thought);
+    getThoughtDetails(thought.id).then((thought) => {
+      console.log(thought);
+      if (thought.children.length > 0) {
+        setParent({
+          id: thought.activeThought.id,
+          name: thought.activeThought.name,
+        });
+        setChildren(thought.children);
+      } else {
+        setErrorMessage(`${thought.activeThought.name} has no children`);
+      }
+    })
+
+  }
+
+  const handleAddThought = (child: Thought, thoughtRelation: ThoughtRelation) => {
+    console.log("Handle add child", child);
+    if (!thoughtCandidate) {
+      return;
+    }
+    getThoughtByExactName(thoughtCandidate.name).then((thought) => {
+      createLink({
+        thoughtIdA: child.id,
+        thoughtIdB: thought.id,
+        relation: thoughtRelation,
+      });
+      if (thoughtRelation === ThoughtRelation.Child) {
+        setChildren([...children, {
+          id: thought.id,
+          name: thoughtCandidate.name,
+          children: [],
+        }]);
+        setThoughtCandidate(null);
+      }
+    }).catch((error) => {
+      console.error("Error creating thought", error);
+      if (error.message === "Thought not found") {
+        console.log("Creating thought");
+        createThought({
+          name: thoughtCandidate.name,
+          sourceThoughtId: child.id,
+          kind: ThoughtKind.Normal,
+          relation: thoughtRelation,
+          acType: AccessType.Private,
+        }).then((thought) => {
+          console.log("Thought created", thought);
+          setThoughtCandidate({
+            ...thoughtCandidate,
+            id: thought.id,
+          });
+          console.log("Thought candidate updated", thought.id);
+          if (thoughtRelation === ThoughtRelation.Child) {
+            setChildren([...children, {
+              id: thought.id,
+              name: thoughtCandidate.name,
+              children: [],
+            }]);
+            setThoughtCandidate(null);
+          }
+        });
+      } else {
+        setErrorMessage("An error occurred while creating the thought");
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="min-h-screen bg-gray-100">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold text-center">Hello World</h1>
+        {/* Parent */}
+        <div className="flex items-center rounded-lg">
+          <div className="relative">
+            {/* Main box */}
+            <div className="border-2 border-black rounded-lg px-8 py-4 hover:bg-gray-200 "
+
+            >
+              {parent.name}
+            </div>
+
+            {/* Bottom button */}
+            <button className="absolute bottom-0 left-1/2 translate-y-4 -translate-x-1/2 w-4 h-4 bg-blue-200 rounded-md flex items-center justify-center hover:bg-blue-300"
+              onClick={() => {
+                handleAddThought(parent, ThoughtRelation.Child);
+              }}
+            >
+              +
+            </button>
+          </div>
+        </div>
+
+        {/* Children */}
+        <div className="flex-wrap flex items-center justify-center gap-8 mt-8 border-2 border-black rounded-lg p-8 m-1">
+          {children.map((child, index) => (
+            <div key={child.id} className="flex items-center rounded-lg">
+              <div className="relative">
+                {/* Left button */}
+                <button className="absolute left-0 top-1/2 -translate-x-4 -translate-y-1/2 w-4 h-4 bg-blue-200 rounded-md flex items-center justify-center hover:bg-blue-300"
+                  onClick={() => {
+                    handleAddThought(child, ThoughtRelation.Jump);
+                  }}
+                >
+                  +
+                </button>
+
+                {/* Main box */}
+                <div className="border-2 border-black rounded-lg px-8 py-4 hover:bg-gray-200 "
+                  onClick={() => {
+                    navigateToThought(child);
+                  }}
+                >
+                  {child.name}
+                </div>
+
+                {/* Bottom button */}
+                <button className="absolute bottom-0 left-1/2 translate-y-4 -translate-x-1/2 w-4 h-4 bg-blue-200 rounded-md flex items-center justify-center hover:bg-blue-300"
+                  onClick={() => {
+                    handleAddThought(child, ThoughtRelation.Child);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          ))}
+
+        </div>
+
+        {/* Thought candidate */}
+        <div className="flex items-center justify-center mt-8">
+          <input
+            type="text"
+            value={thoughtCandidate?.name || ""}
+            onChange={(e) => setThoughtCandidate({
+              ...thoughtCandidate,
+              name: e.target.value
+            })}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleAddThought(parent, ThoughtRelation.Child);
+              }
+            }}
+            className="border-2 border-black rounded-lg px-8 py-4 focus:outline-none focus:border-blue-500"
+            placeholder="Enter thought name"
+          />
+          <button
+            className="bg-blue-500 text-white mx-2 px-8 py-4 rounded-md"
+            onClick={() => {
+              handleAddThought(parent, ThoughtRelation.Child);
+            }}
+          >Add</button>
+
+        </div>
+
+
+        {errorMessage && <div className="text-red-500 text-center mt-8">{errorMessage}</div>}
+
+      </div>
+    </div >
   );
 }
 
---
+++ src/Client.ts
@@ -0,0 +1,175 @@
+export enum ThoughtKind {
+    Normal = 1,
+    Type = 2,
+    Event = 3,
+    Tag = 4,
+    System = 5
+}
+
+export enum ThoughtRelation {
+    Child = 1,
+    Parent = 2,
+    Jump = 3,
+    Sibling = 4
+}
+
+export enum AccessType {
+    Public = 0,
+    Private = 1
+}
+
+export const API_KEY = "e521ea99e1b5b574c3eab5a13208ebe0b929712da46abbcb150df362a5f7faab"
+export const BRAIN_ID = "b0f66acd-1357-4175-9ab7-74b620b637d2"
+export const API_BASE_URL = "https://api.bra.in"
+export const ROOT_THOUGHT_ID = "5af76133-1c66-4136-8a50-9a7810385e92"
+
+
+
+export const getBrains = async () => {
+    const response = await fetch(`${API_BASE_URL}/brains`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Request failed: ' + response.statusText);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export const getThoughtDetails = async (thoughtId: string, includeSiblings: boolean = false) => {
+    const response = await fetch(
+        `${API_BASE_URL}/thoughts/${BRAIN_ID}/${thoughtId}/graph?includeSiblings=${includeSiblings}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Request failed: ' + response.statusText);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export const getThoughtByExactName = async (nameExact: string) => {
+    const response = await fetch(`${API_BASE_URL}/thoughts/${BRAIN_ID}?nameExact=${encodeURIComponent(nameExact)}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.status === 404) {
+        throw new Error("Thought not found");
+    }
+
+    if (!response.ok) {
+        throw new Error('Request failed: ' + response.statusText);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export interface CreateThoughtParams {
+    name: string;
+    kind: ThoughtKind;
+    label?: string;
+    typeId?: string;
+    sourceThoughtId?: string;
+    relation?: ThoughtRelation;
+    acType?: AccessType;
+}
+
+export const createThought = async (params: CreateThoughtParams) => {
+
+    try {
+        const response = await fetch(`${API_BASE_URL}/thoughts/${BRAIN_ID}`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: params.name,
+                kind: params.kind,
+                label: params.label,
+                typeId: params.typeId,
+                sourceThoughtId: params.sourceThoughtId,
+                relation: params.relation,
+                acType: params.acType
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Request failed: ' + response.statusText);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error: any) {
+        console.error('Error: ', error.message);
+        throw new Error('An error occurred while creating the thought.');
+    }
+};
+
+
+export const getThought = async (thoughtId: string) => {
+    const response = await fetch(`${API_BASE_URL}/thoughts/${BRAIN_ID}/${thoughtId}`, {
+        method: 'GET',
+    });
+
+    if (!response.ok) {
+        throw new Error('Request failed: ' + response.statusText);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export interface CreateLinkParams {
+    thoughtIdA: string;
+    thoughtIdB: string;
+    relation: ThoughtRelation;
+    name?: string;
+}
+
+export const createLink = async (params: CreateLinkParams) => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/links/${BRAIN_ID}`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                thoughtIdA: params.thoughtIdA,
+                thoughtIdB: params.thoughtIdB,
+                relation: params.relation,
+                name: params.name
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Request failed: ' + response.statusText);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error: any) {
+        console.error('Error: ', error.message);
+        throw new Error('An error occurred while creating the link.');
+    }
+};
+
+
